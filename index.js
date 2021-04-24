8a4d4d5618292e1070da9b99d6a7463270e632a9
LAPTOP-9ENHBV1Q\Sri <puneethrgowda43@gmail.com>
2021-04-24 15:02:34 +0530

    changed msg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({msg:'express server running'})
+    res.send({msg:'express server deployment successfull'})
 })
 
 const PORT = process.env.PORT || 5000
